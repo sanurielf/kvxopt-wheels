e2bbee3b21b349db1f0897231d2934a4c2d57ba3
Uriel Sandoval <sanurielf@gmail.com>
2021-04-02 14:14:56 -0600

    Add test to upload to pypi

--- library_builders.sh
+++ library_builders.sh
@@ -94,14 +94,14 @@ function build_osqp {
   if [ -n "${IS_OSX}" ]; then
       brew install osqp
   else
-  get_cmake_320
-  git clone --recursive https://github.com/oxfordcontrol/osqp.git
-  (cd osqp \
-      && git checkout v${OSQP_VERSION} \
-      && mkdir build \
-      && cd build \
-      && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX .. \
-      && cmake --build . --target install)
+    get_modern_cmake
+    git clone --recursive https://github.com/oxfordcontrol/osqp.git
+    (cd osqp \
+        && git checkout v${OSQP_VERSION} \
+        && mkdir build \
+        && cd build \
+        && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX .. \
+        && cmake --build . --target install)
 fi
   touch osqp-stamp
 }
