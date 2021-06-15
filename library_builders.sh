9d1be57586ca4be9f90470f21976854d0739e9a5
Uriel Sandoval <sanurielf@gmail.com>
2021-06-15 18:22:02 -0500

    Compile openblas from source for OSx

--- library_builders.sh
+++ library_builders.sh
@@ -9,6 +9,8 @@ FFTW_SHA256="6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303"
 SUITESPARSE_VERSION="5.7.2"
 SUITESPARSE_SHA256="fe3bc7c3bd1efdfa5cffffb5cebf021ff024c83b5daf0ab445429d3d741bd3ad"
 OSQP_VERSION="0.6.2"
+OPENBLAS_VERSION="0.3.10"
+OPENBLAS_SHA256="0484d275f87e9b8641ff2eecaa9df2830cbe276ac79ad80494822721de6e1693"
 
 
 type fetch_unpack &> /dev/null || source multibuild/library_builders.sh
@@ -42,7 +44,46 @@ function build_dsdp {
   touch dsdp-stamp
 }
 
-function build_openblas_os {
+function build_openblas_osx {
+  if [ -e openblas_osx-stamp ]; then return; fi
+  
+  if [ "${PLAT}" == "arm64" ]; then
+    brew uninstall openblas
+  fi
+  (brew install openblas \
+    && brew link --force openblas)
+  
+
+  touch openblas_osx-stamp
+  
+}
+
+function openblas_get_osx {
+    # Get an openblas compiled library from
+    # https://anaconda.org/multibuild-wheels-staging/openblas-libs
+    # The general form of the link is (under the URL above)
+    # URL/v0.3.10/downloads/openblas-v0.3.10-manylinux2010_x86_64.tar.gz
+    local plat=${1:-$}
+    # qual could be 64 to get a 64-bit version
+    local qual=$2
+    local prefix=openblas${qual}-v$OPENBLAS_VERSION
+    local manylinux=manylinux${MB_ML_VER:-1}
+    local fname="$prefix-${manylinux}_${plat}.tar.gz"
+    local out_fname="${ARCHIVE_SDIR}/$fname"
+    if [ ! -e "$out_fname" ]; then
+        local webname=${OPENBLAS_LIB_URL}/v${OPENBLAS_VERSION}/download/${fname}
+        curl -L "$webname" > $out_fname || exit 1
+        # make sure it is not an HTML document of download failure
+        local ok=$(file $out_fname | grep "HTML document")
+        if [ -n "$ok" ]; then
+            echo Fetching "${OPENBLAS_LIB_URL}/$fname" failed;
+            exit 1;
+        fi
+    fi
+    echo "$out_fname"
+}
+
+function build_openblas_osx2 {
     if [ -e openblas-stamp ]; then return; fi
     if [ -n "$IS_MACOS" ]; then
         brew install openblas
@@ -55,6 +96,7 @@ function build_openblas_os {
     fi
     touch openblas-stamp
 }
+
 
 
 function build_fftw {
@@ -90,12 +132,22 @@ function build_gsl {
 
   fetch_unpack http://ftp.download-by.net/gnu/gnu/gsl/gsl-${GSL_VERSION}.tar.gz
   check_sha256sum archives/gsl-${GSL_VERSION}.tar.gz ${GSL_SHA256}
-  (cd gsl-${GSL_VERSION} \
+
+  if [ -n "$IS_MACOS" ]; then
+      (cd gsl-${GSL_VERSION} \
       && ./configure --prefix=${BUILD_PREFIX} \
       && make \
       && make install \
       && cd .. \
       && rm -rf gsl-${GSL_VERSION})
+  else
+      (cd gsl-${GSL_VERSION} \
+      && ./configure --prefix=${BUILD_PREFIX}\
+      && make \
+      && make install \
+      && cd .. \
+      && rm -rf gsl-${GSL_VERSION})
+  fi
   touch gsl-stamp
 }
 
@@ -110,7 +162,7 @@ function build_osqp {
       && cd build \
       && cmake -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX .. \
       && cmake --build . --target install \
-      && cd .. \
+      && cd ../.. \
       && rm -rf osqp)
 
   touch osqp-stamp
